d375ac30f81ec22fdea0270f0ff44190aba26847
dani-ryd <dani.ruhyadi01@gmail.com>
2021-06-12 20:09:25 +0700

    quiz 13

---
+++ app/src/main/java/com/app/firebasecrashlytics/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.app.firebasecrashlytics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val crashButton = Button(this)
+        crashButton.text = "Crash!"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
+    }
+}
